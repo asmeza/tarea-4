21097c502470b32afbb7dc731be1bdb8606fef2f
Alan Meza <asmeza@uniguajira.edu.co>
2020-09-16 17:02:11 -0500

    creacion menu basico completo

--- menu.py
+++ menu.py
@@ -1,2 +1,58 @@
+import os
+
+
+def problema1():
+    print("problema1")
+
+def problema2():
+    print("problema2")
+
+def problema3():
+    print("problema3")
+
+
+def menu():
+    os.system("pause")
+    os.system("cls")
+    
+    while(True):
+        print(" ___________________________________")
+        print("|    Servicio de  Estado de Cuenta  |")
+        print("|                                   |")
+        print("|  ¿Que deseas hacer?               |")
+        print("|                                   |")
+        print("|  1. problema1                     |")
+        print("|  2. problema2                     |")
+        print("|  3. problema3                     |")
+        print("|  4. salir                         |")
+        print("|                                   |")
+        print("|   por favor elige una opción      |")
+        print("|                                   |")
+        print("|___________________________________|")
+
+        opc= int(input("elige una opcion => "))
+        if opc == 1:
+            problema1()
+        elif opc == 2:
+            problema2()
+        elif opc == 3:
+            problema3()  
+        elif opc== 4:
+            print("hasta luego")
+            break
+        else:
+            print("hasta luego")
+            break
+   
+        os.system("pause")
+        os.system("cls")
+
+
+
+
+def main():
+    menu()
+
 if __name__ == "__main__":
     pass
+    main()
